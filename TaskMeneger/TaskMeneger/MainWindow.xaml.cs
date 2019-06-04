65c168d03c6b6605a58ef0ec9d213dcc87bda88d
BarakpaevOlzhas <40425093+BarakpaevOlzhas@users.noreply.github.com>
2019-06-04 18:27:51 +0600

    Add files via upload

---
+++ TaskMeneger/TaskMeneger/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+
+namespace TaskMeneger
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        System.Windows.Controls.Primitives.Popup menu;
+
+        bool IsExit = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            typeSelect.Items.Add("Почта");
+            typeSelect.Items.Add("Скачать файл");
+            typeSelect.Items.Add("Перенести каталог");
+
+            System.Windows.Forms.NotifyIcon ni = new System.Windows.Forms.NotifyIcon();
+            ni.Icon = new System.Drawing.Icon("Icon.ico");
+            ni.Visible = true;
+            ni.Click += ClickNi;
+
+            this.Closing += ThisClosing;
+
+        }
+
+        private void ClickNi(object sender, EventArgs e)
+        {
+            Visibility = Visibility.Visible;
+        }
+
+        private void ThisClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!IsExit)
+            {
+                e.Cancel = true;
+                this.Visibility = Visibility.Collapsed;
+            }
+            else
+                e.Cancel = false;
+        }
+
+        private void ButtonClickExit(object sender, RoutedEventArgs e)
+        {
+            IsExit = true;
+            this.Close();
+        }
+
+        private void ComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ TaskMeneger/TaskMeneger/TaskAction.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMeneger
+{
+    public class TaskAction
+    {
+        private Task task;
+
+        public TaskAction(Task taskTwo)
+        {
+            task = taskTwo;
+        }
+
+        public void SendMessages()
+        {
+            var fromAddress = new MailAddress("olzhas.barakpaev@gmail.com");
+            var toAddress = new MailAddress("olzhas.barakpaev@gmail.com");
+            const string fromPassword = "titivi032312";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            smtp.EnableSsl = true;
+            using (var message = new MailMessage(fromAddress, toAddress)
+            { Subject = task.HeadMessage, Body = task.BodyMessage })
+            {
+                smtp.Send(message);
+            }
+        }
+
+        public void DownloadFile()
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(task.FromDownloadPath, task.NameFile);
+            }
+        }
+
+        public void MoveCaralog()
+        {
+            DirectoryInfo dir = new DirectoryInfo(task.Catalog);
+            dir.MoveTo(task.MovePath);
+        }
+    }
+}
